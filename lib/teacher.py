f23974a627328a55e5c8bb663d827df5bb57a293
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-06-28 23:10:00 +0300

    Parent class and children class set up and configured

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,27 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+                            "str is a data type in Python",
+                            "programming is hard, but it's worth it",
+                            "JavaScript async web request",
+                            "Python function call definition",
+                            "object-oriented teacher instance",
+                            "programming computers hacking learning terminal",
+                            "pipenv install pipenv shell",
+                            "pytest -x flag to fail fast",
+                        ]
 
     def teach(self):
-        pass
+        # Create a random index of the array passing the array length as the range
+        random_index = random.randint(0,len(self.knowledge)-1)
+        print(random_index)
+        return self.knowledge[random_index]
+
+test = Teacher("Eric", "Brian")
+print(test.first_name)
+print(test.last_name)
+print(test.knowledge)
+test.teach()
